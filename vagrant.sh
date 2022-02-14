20c7b09e849fee6a3252daeb36942e306fb6e11d
cielavenir <cielartisan@gmail.com>
2022-02-14 15:30:44 +0900

    fix omnibus install.sh URL

--- vagrant.sh
+++ vagrant.sh
@@ -1,7 +1,10 @@
+# OMNIBUS_URL="https://www.opscode.com/chef/install.sh"
+OMNIBUS_URL="https://omnitruck.chef.io/install.sh"
+
 vagrant ssh-config --host mujin > ./mujin_sshconfig
 #ssh -F ./mujin_sshconfig mujin
 #berks vendor vendor/cookbooks
 if ! vagrant ssh -c 'dpkg -L chef' >/dev/null; then
-  knife solo prepare -F ./mujin_sshconfig mujin
+  knife solo prepare -F ./mujin_sshconfig mujin --omnibus-url "${OMNIBUS_URL}"
 fi
 knife solo cook -F ./mujin_sshconfig mujin
