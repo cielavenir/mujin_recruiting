7284c67e9102caf4e9285bd00e479cd1267af42a
cielavenir <cielartisan@gmail.com>
2022-02-14 19:01:48 +0900

    fix vagrant_buster omnibus_url

--- vagrant_buster.sh
+++ vagrant_buster.sh
@@ -1,7 +1,10 @@
+# OMNIBUS_URL="https://www.opscode.com/chef/install.sh"
+OMNIBUS_URL="https://omnitruck.chef.io/install.sh"
+
 vagrant ssh-config --host buster > ./buster_sshconfig
 #ssh -F ./buster_sshconfig buster
 #berks vendor vendor/cookbooks
 if ! vagrant ssh -c 'dpkg -L chef' >/dev/null; then
-  knife solo prepare -F ./buster_sshconfig buster
+  knife solo prepare -F ./buster_sshconfig buster --omnibus-url "${OMNIBUS_URL}"
 fi
 knife solo cook -F ./buster_sshconfig buster
